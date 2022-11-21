9c35179d203eb2cf1c91f62257a0be467ce07cf6
JianHua-Deng <dengjianhua2397462613@gmail.com>
2022-11-21 11:48:55 -0500

    Adde test cases for task B

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "profile.h"
+#include "network.h"
 //#include "funcs.h"
 
 TEST_CASE("Profile class"){
@@ -11,4 +12,19 @@ TEST_CASE("Profile class"){
     p1.setDisplayName("Marco Rossi"); 
     CHECK(p1.getFullName() == "Marco Rossi (@marco)");
 
+}
+
+TEST_CASE("Network class"){
+    Network nw;
+    CHECK(nw.addUser("mario", "Mario") == true);    // true (1)
+    CHECK(nw.addUser("luigi", "Luigi") == true);     // true (1)
+
+    CHECK(nw.addUser("mario", "Mario2") == false);    // false (0)
+    CHECK(nw.addUser("mario 2", "Mario2") == false);  // false (0)
+    CHECK(nw.addUser("mario-2", "Mario2") == false);  // false (0)
+
+    for(int i = 2; i < 20; i++){
+        CHECK(nw.addUser("mario" + std::to_string(i), "Mario" + std::to_string(i)) == true);   // true (1)
+    }//end for loop
+    CHECK(nw.addUser("yoshi", "Yoshi") == false);     // false (0)
 }
