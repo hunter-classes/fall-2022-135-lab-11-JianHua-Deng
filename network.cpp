b20a33664edcd0549bcdecc09d1f1214eaaa0175
JianHua-Deng <dengjianhua2397462613@gmail.com>
2022-11-21 12:24:23 -0500

    Task C work in progress

--- network.cpp
+++ network.cpp
@@ -2,7 +2,14 @@
 
 //Constructor
 Network::Network(){
+    //Task B
     numUsers = 0;
+    //Task C making following 2d array to be false
+    for(int i = 0; i < MAX_USERS; i++){
+        for(int j = 0; j < MAX_USERS; j++){
+            following[i][j] = false;
+        }//end inner for loop
+    }//end for loop
 }//end Network constructor
 
 // Attempts to sign up a new user with specified username and displayname
@@ -22,4 +29,17 @@ bool Network::addUser(std::string usrn, std::string dspn){
         return true;
     }//end if condition
     return false;
+
 }//end addUser function
+
+ // Make 'usrn1' follow 'usrn2' (if both usernames are in the network).
+ // return true if success (if both usernames exist), otherwise return false
+ bool Network::follow(std::string usrn1, std::string usrn2){
+    //if both username exists in the network
+    if(findID(usrn1) != -1 && findID(usrn2) != -1){
+        following[findID(usrn1)][findID(usrn2)] = true;
+        return true;
+    }else{
+        return false;
+    }//end else codnition
+ }//end follow function
--- network.h
+++ network.h
@@ -5,6 +5,8 @@
 class Network {
 private:
   static const int MAX_USERS = 20; // max number of user profiles
+  bool following[MAX_USERS][MAX_USERS];  // friendship matrix:
+  // following[id1][id2] == true when id1 is following id2
   int numUsers;                    // number of registered users
   Profile profiles[MAX_USERS];     // user profiles array:
                                    // mapping integer ID -> Profile
@@ -25,4 +27,6 @@ public:
   // Attempts to sign up a new user with specified username and displayname
   // return true if the operation was successful, otherwise return false
   bool addUser(std::string usrn, std::string dspn);
+  bool follow(std::string usrn1, std::string usrn2);
+  void printDot();
 };
